0a70892b79f1bf85924b5d48af4f606b7832518d
naoto <micuat@gmail.com>
2016-09-21 13:02:31 -0400

    Added tips to save wav files

--- matlab/mmn_shapes_experiment.m
+++ matlab/mmn_shapes_experiment.m
@@ -3,3 +3,7 @@ clear
 % SOA, tDur, number of stimuli, pDeviant
 m = mmn_shapes(0.749+0.169, 0.169, 100, 0.2)
 start(m)
+
+% save audio files
+% audiowrite('standard.wav', m.wavStandard, m.Fs);
+% audiowrite('deviant.wav', m.wavDeviant, m.Fs);
