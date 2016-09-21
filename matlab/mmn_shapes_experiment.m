1718617b5dc923fefaf562d5588b1853a0e65818
naoto <micuat@gmail.com>
2016-09-21 14:40:35 -0400

    change number of stimuli

--- matlab/mmn_shapes_experiment.m
+++ matlab/mmn_shapes_experiment.m
@@ -1,7 +1,7 @@
 clear
 
 % SOA, tDur, number of stimuli, pDeviant
-m = mmn_shapes(0.749+0.169, 0.169, 100, 0.2)
+m = mmn_shapes(0.749+0.169, 0.169, 500, 0.2)
 start(m)
 
 % save audio files
--- matlab/mmn_sound_experiment.m
+++ matlab/mmn_sound_experiment.m
@@ -1,5 +1,5 @@
 clear
 
 % SOA, tDur, number of stimuli, pDeviant
-m = mmn_sound(0.749+0.169, 0.169, 100, 0.2)
+m = mmn_sound(0.749+0.169, 0.169, 500, 0.2)
 start(m)
