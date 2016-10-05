b944a11e13bab06af7688b7e775ff773df46adb2
naoto <micuat@gmail.com>
2016-10-05 17:33:27 -0400

    Fix audio save command to pad zero for avoiding aliasing

--- matlab/mmn_shapes_experiment.m
+++ matlab/mmn_shapes_experiment.m
@@ -3,7 +3,3 @@ clear
 % SOA, tDur, number of stimuli, pDeviant
 m = mmn_shapes(0.749+0.169, 0.169, 500, 0.2)
 start(m)
-
-% save audio files
-% audiowrite('standard.wav', m.wavStandard, m.Fs);
-% audiowrite('deviant.wav', m.wavDeviant, m.Fs);
--- matlab/mmn_sound_experiment.m
+++ matlab/mmn_sound_experiment.m
@@ -3,3 +3,8 @@ clear
 % SOA, tDur, number of stimuli, pDeviant
 m = mmn_sound(0.749+0.169, 0.169, 500, 0.2)
 start(m)
+
+% save audio files
+% notice 500ms (Fs / 2 samples) of paddings
+% audiowrite('standard.wav', [zeros(1, m.Fs / 2) m.wavStandard * 0.5 zeros(1, m.Fs / 2)], m.Fs);
+% audiowrite('deviant.wav', [zeros(1, m.Fs / 2) m.wavDeviant * 0.5 zeros(1, m.Fs / 2)], m.Fs);
