a36f4350cb10b0a427d11a2633abc1ee07dac16b
micuat <micuat@gmail.com>
2016-11-03 13:21:48 -0400

    Plot multi channel

--- python/lsl_emokit_receiver.py
+++ python/lsl_emokit_receiver.py
@@ -14,7 +14,7 @@ inlet = StreamInlet(streams[0])
 info = inlet.info()
 print("number of channels: %d" % info.channel_count())
 
-buf = np.zeros((128 * 5))
+buf = np.zeros((128 * 2, info.channel_count()))
 
 while True:
     # get a new sample (you can also omit the timestamp part if you're not
@@ -22,8 +22,10 @@ while True:
     chunk, timestamps = inlet.pull_chunk()
     if timestamps:
         for vec in chunk:
-            buf = np.append(buf, vec[0])
-        buf = buf[-128 * 5:]
+            buf = np.append(buf, np.matrix(vec), axis=0)
+        buf = buf[-128 * 2:, :]
         plt.clf()
-        plt.plot(buf)
-        plt.pause(0.05)
+        for i in range(0, info.channel_count()):
+            offset = i * 10
+            plt.plot(buf[:, i] - np.mean(buf[:, i] + offset))
+        plt.pause(0.01)
